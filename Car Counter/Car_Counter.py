eb4965a036fc35d17def83e3d3df7b55e204ee09
gabecodex <gabrielfmdesouza@gmail.com>
2023-02-12 11:02:25 -0500

    Mask video capture and model

--- Car Counter/Car_Counter.py
+++ Car Counter/Car_Counter.py
@@ -4,3 +4,21 @@ import cv2
 import cvzone
 import math
 from sort import *
+
+cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+
+model = YOLO("../Yolo-Weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+mask = cv2.imread("mask.png")
